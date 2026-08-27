f40f899fb01f676928e4c4c11e1f597a9f554c03
kimaniks001 <jkwanjohi@gmail.com>
2026-08-27 16:24:54 +0300

    refactor: isolate edge-safe SecurePay cookie names

---
+++ src/lib/community/securepay-session-names.ts
@@ -0,0 +1,4 @@
+export const SECUREPAY_ACCESS_COOKIE = "outreach_securepay_access";
+export const SECUREPAY_REFRESH_COOKIE = "outreach_securepay_refresh";
+export const SECUREPAY_KS_HINT_COOKIE = "outreach_securepay_ks_hint";
+export const SECUREPAY_PENDING_KS_COOKIE = "outreach_securepay_pending_ks";
